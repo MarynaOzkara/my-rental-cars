2384787b1dfbd85433711d2d9c5182476356f4cd
MarynaOzkara <126406516+MarynaOzkara@users.noreply.github.com>
2024-02-02 15:19:55 +0000

    Deploying to gh-pages from @ MarynaOzkara/my-rental-cars@2e83852ae8e47f4d45bd5088f84ccc3c8fbfc95e 🚀

--- static/js/842.2c8bc3f9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_rental_cars=self.webpackChunkmy_rental_cars||[]).push([[842],{7842:function(e,n,r){r.r(n);r(2791);var c=r(184);n.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:"Home"})})}}}]);
-//# sourceMappingURL=842.2c8bc3f9.chunk.js.map
